3a3e14367f9155a2717853fe14af92399f06dd15
Norp <github.20.lumiken@spamgourmet.com>
2014-12-03 16:45:48 +0100

    traversing tree is no possible during the parse. No tree listener parsing though

--- src/test/java/com/constantiuous/structypus/AntlrTest.java
+++ src/test/java/com/constantiuous/structypus/AntlrTest.java
@@ -3,6 +3,7 @@ package com.constantiuous.structypus;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Stack;
 
 import org.antlr.runtime.tree.CommonTree;
@@ -26,22 +27,30 @@ import org.antlr.v4.runtime.tree.RuleNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import org.junit.Test;
 
+import com.foo.antlr.ComponentFunction;
 import com.foo.antlr.Java8BaseListener;
 import com.foo.antlr.Java8Lexer;
 import com.foo.antlr.Java8Parser;
+import com.foo.antlr.Java8Parser.ClassBodyDeclarationContext;
+import com.foo.antlr.Java8Parser.ImportDeclarationContext;
+import com.foo.antlr.Java8Parser.MethodDeclarationContext;
+import com.foo.antlr.Java8Parser.NormalClassDeclarationContext;
+import com.foo.antlr.Java8Parser.SingleTypeImportDeclarationContext;
 import com.foo.antlr.Java8Parser.VariableDeclaratorContext;
 
 public class AntlrTest {
 
 	@Test
 	public void testParseFile() throws Exception {
+		System.out.println("------------------------------");
 		String filePath = TestProperties.RES_JAVA
 				+ "com/faa/ImportsSimpleFile.java";
 		parseFile(filePath);
 	}
 
-	
+	@Test
 	public void testExampleField() throws Exception {
+		System.out.println("------------------------------");
 		String filePath = TestProperties.RES_JAVA
 				+ "com/faa/ImportsSimpleFile.java";
 
@@ -58,7 +67,7 @@ public class AntlrTest {
 
 		System.out.println("CompilationUnit: " + p.compilationUnit());
 		System.out.println("Packagename: " + p.packageName());
-
+		
 		System.out.println(p);
 	}
 
@@ -177,52 +186,80 @@ public class AntlrTest {
 		public void visitTerminal(TerminalNode ctx) {
 			// TODO Auto-generated method stub
 			
-			System.out.println("visitTerminal: ["+ctx.getText()+"] Source Interval: ["+ctx.getSourceInterval()+"] child count: ["+ctx.getChildCount()+"] Line ["+ctx.getSymbol().getLine()+"]");
+			System.out.println("visitTerminal: ["+ctx+"] Text: ["+ctx.getText()+"] Source Interval: ["+ctx.getSourceInterval()+"] child count: ["+ctx.getChildCount()+"] Line ["+ctx.getSymbol().getLine()+"]");
+			
 		}
 
 		
 	}
 	
-	public  static class AnnListener extends Java8BaseListener {
-
+	public static class AnnListener extends Java8BaseListener {
+		public HashMap<String, ComponentFunction> functions;
+		private MethodDeclarationContext currentMethod;
+		
+		public AnnListener() {
+			functions = new HashMap<String, ComponentFunction>();
+		}
+		
 		@Override
-		public void enterMethodInvocation_lf_primary(
-				@NotNull Java8Parser.MethodInvocation_lf_primaryContext ctx) {
-			System.out.println("Method invocation lf enter: "
-					+ ctx.getText());
+		public void enterMethodDeclaration(MethodDeclarationContext ctx) {
+			System.out.println("<<Method Declaration enter: "
+					+ ctx.methodHeader());
+			currentMethod = ctx;
+		}
+		
+		@Override
+		public void exitMethodDeclaration(MethodDeclarationContext ctx) {
+			System.out.println("<<Method Declaration exit: "
+					+ ctx.methodHeader().methodDeclarator().getText());
+			if(currentMethod == ctx){
+				System.out.println("Same Method Context!!!");
+			}
+			
+			
+//			String functionName = ctx.methodName().getText();
+//			currentFunction = functionName;
+//			ComponentFunction function = new ComponentFunction(functionName);
+//			functions.put(functionName, function);
 		}
 
 		@Override
-		public void enterClassBodyDeclaration(
-				@NotNull Java8Parser.ClassBodyDeclarationContext ctx) {
-			System.out.println("Class body declaration enter: "
-					+ ctx.getText());
-
+		public void exitImportDeclaration(ImportDeclarationContext ctx) {
+			if(ctx.singleTypeImportDeclaration() != null){
+				System.out.println("Single Type Import Declaration exit: "
+						+ ctx.singleTypeImportDeclaration().typeName().getText());
+			}
+			else if(ctx.typeImportOnDemandDeclaration() != null){
+				System.out.println("Type Import OnDemand Declaration exit: "
+						+ ctx.typeImportOnDemandDeclaration().packageOrTypeName().getText());
+			}
+			else if(ctx.singleStaticImportDeclaration() != null){
+				System.out.println("Single Static Import Declaration exit: "
+						+ ctx.singleStaticImportDeclaration().typeName().getText());
+			}
+			else if(ctx.staticImportOnDemandDeclaration() != null){
+				System.out.println("Static Import OnDemand Declaration exit: "
+						+ ctx.staticImportOnDemandDeclaration().typeName().getText());
+			}
 		}
 
 		@Override
-		public void enterTypeImportOnDemandDeclaration(
-				@NotNull Java8Parser.TypeImportOnDemandDeclarationContext ctx) {
-			System.out.println("Type Import on Demand enter: "
-					+ ctx.getText());
-
+		public void enterNormalClassDeclaration(NormalClassDeclarationContext ctx) {
+			// enum classes are non-normal
+			System.out.println("<Normal Class Declaration enter: " + ctx.Identifier());
 		}
 
 		@Override
-		public void enterClassBody(
-				@NotNull Java8Parser.ClassBodyContext ctx) {
-			System.out.println("Class Body enter: " + ctx.getText());
-//			System.out.println("Class Body enter: " + ctx.Identifier().getText());
-
-			// System.out.println("Class Body enter: Declaration: "+ctx.classBodyDeclaration());
-
+		public void exitNormalClassDeclaration(NormalClassDeclarationContext ctx) {
+			// enum classes are non-normal
+			System.out.println("<Normal Class Declaration exit: " + ctx.Identifier());
 		}
 
 		@Override
-		public void enterMethodInvocation(
+		public void exitMethodInvocation(
 				@NotNull Java8Parser.MethodInvocationContext ctx) {
-			System.out.println("Method invocation enter: "
-					+ ctx.getText());
+			System.out.println("<<Method invocation exit: "
+					+ ctx.methodName().getText());
 
 		}
 	}
